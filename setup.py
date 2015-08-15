fdfea6854f9919db0c5595ab311148e427064c32
Kristian Øllegaard <kristian@kristian.io>
2015-08-15 20:20:48 +0200

    moved __main__ to cli and fixed entry point

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'app-image-generator = app_image_generator.__main__',
+            'app-image-generator = app_image_generator.cli:main',
         ]
     },
 )
