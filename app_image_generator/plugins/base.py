65faa964f6b3514b206e5301eb4597459564a664
Ales Kocjancic <alesdotio@gmail.com>
2017-01-31 13:10:28 +0100

    fixed regex result to be serializable

--- app_image_generator/plugins/base.py
+++ app_image_generator/plugins/base.py
@@ -4,7 +4,7 @@ import re
 class BasePlugin(object):
 
     def get_images_from_output(self, output):
-        return dict(re.findall(b"\-\-> (\w+): [\w ]+:\n\n[\w-]+: ([\w-]+)", output))
+        return {k.decode("utf-8"): i.decode("utf-8") for k, i in re.findall(b"\-\-> (\w+): [\w ]+:\n\n[\w-]+: ([\w-]+)", output)}
 
     def build_succeeded(self, output):
         pass
