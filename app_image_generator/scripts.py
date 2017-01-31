b7b5e6f24042d529954054fefa8a8306965a4a4f
Kristian Øllegaard <kristian@kristian.io>
2017-01-31 10:47:51 +0100

    fix for python 2.x

--- app_image_generator/scripts.py
+++ app_image_generator/scripts.py
@@ -33,7 +33,10 @@ def run(base_ami, version, revision, git_revision, deployment_file, app, base_am
             p.stdin.close()
             for line in iter(p.stdout.readline, b''):
                 output += line
-                print(line, flush=True)  # Don't add a line break, as it's already provided by the output
+                try:
+                    print(line, flush=True)  # Don't add a line break, as it's already provided by the output
+                except TypeError:  # Python<3.5
+                    print(line)
             p.stdout.close()
             return_code = p.wait()
 
