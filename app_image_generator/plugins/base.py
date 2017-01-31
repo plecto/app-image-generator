c72725bec71343d510f4ff101f98d96da7c2facc
Ales Kocjancic <alesdotio@gmail.com>
2017-01-31 11:04:12 +0100

    fixed regex on python3

--- app_image_generator/plugins/base.py
+++ app_image_generator/plugins/base.py
@@ -4,7 +4,7 @@ import re
 class BasePlugin(object):
 
     def get_images_from_output(self, output):
-        return dict(re.findall(r"\-\-> (\w+): [\w ]+:\n\n[\w-]+: ([\w-]+)", output))
+        return dict(re.findall(b"\-\-> (\w+): [\w ]+:\n\n[\w-]+: ([\w-]+)", output))
 
     def build_succeeded(self, output):
         pass
